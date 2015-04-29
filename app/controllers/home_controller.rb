2c355b3921c932ebc152425bf25e136dfd663952
Juan Carlos <krlos013@gmail.com>
2015-04-28 20:17:00 -0500

    8

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
   	@name = Profile.where(:user_id => current_user.id).count
   	if @name != 0 
-     @mensaje = "Lo sentimos, el código ingresado no es valido, favor de ingresar un código valido."
+    
     else
      redirect_to :controller => 'profiles', :action => 'new'
     
