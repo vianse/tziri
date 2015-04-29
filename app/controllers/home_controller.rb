bf52baa486fb71c1f82690569bee949042dedcc7
Juan Carlos <krlos013@gmail.com>
2015-04-28 20:19:13 -0500

    9

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
   def index
-  	@name = Profile.where(:user_id => current_user.id).count
-  	if @name != 0 
+  	# @name = Profile.where(:user_id => current_user.id).count
+  	# if @name != 0 
     
-    else
-     redirect_to :controller => 'profiles', :action => 'new'
+   #  else
+   #   redirect_to :controller => 'profiles', :action => 'new'
     
-    end
+   #  end
   end
 end
